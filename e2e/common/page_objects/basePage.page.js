6e5e53a4c57395a1a87ae8b730a9fabbc4e5cf21
Quintus Chen <quintus.chen@vizexplorer.com>
2018-05-03 12:03:54 +1200

    Refactor the structure a bit thanks to Wim

--- e2e/common/page_objects/basePage.page.js
+++ e2e/common/page_objects/basePage.page.js
@@ -1,17 +1,24 @@
+import {
+    ANDROID_ALERT_MESSAGE_SELECTOR,
+    IOS_ALERT_SELECTOR,
+    IOS_TEXT_SELECTOR,
+    ANDROID_ACCEPT_ALERT_SELECTOR
+} from '../../../support/constants';
+
 export default class BasePage {
 
     get alertOKButton() {
-        return device.isIOS ? $('//*[@name="OK"]') : $('//*[@resource-id="android:id/button1"]')
+        return device.isIOS ? $(IOS_ACCEPT_ALERT_SELECTOR) : $(ANDROID_ACCEPT_ALERT_SELECTOR);
     }
 
     getAlertMessage() {
         if (device.isIOS) {
-            device.waitForExist('XCUIElementTypeAlert');
-            let alert = $('XCUIElementTypeAlert');
-            return alert.getText('XCUIElementTypeStaticText');
+            device.waitForExist(IOS_ALERT_SELECTOR);
+            let alert = $(IOS_ALERT_SELECTOR);
+            return alert.getText(IOS_TEXT_SELECTOR);
         } else if (device.isAndroid) {
-            device.waitForExist('//*[@resource-id="android:id/message"]');
-            return device.getText('//*[@resource-id="android:id/message"]');
+            device.waitForExist(ANDROID_ALERT_MESSAGE_SELECTOR);
+            return device.getText(ANDROID_ALERT_MESSAGE_SELECTOR);
         }
     }
 
---
+++ support/constants.js
@@ -0,0 +1,11 @@
+/**
+ * Cross-platform Text selectors
+ */
+
+export const ANDROID_TEXT_SELECTOR = '*//android.widget.TextView';
+export const ANDROID_ALERT_TITLE_SELECTOR = '*//android.widget.TextView[@resource-id="android:id/alertTitle"]';
+export const ANDROID_ALERT_MESSAGE_SELECTOR = '*//android.widget.TextView[@resource-id="android:id/message"]';
+export const ANDROID_ACCEPT_ALERT_SELECTOR = '*//android.widget.Button[@text="OK"]';
+export const IOS_ACCEPT_ALERT_SELECTOR = '//*[@name="OK"]';
+export const IOS_ALERT_SELECTOR = '*//XCUIElementTypeAlert';
+export const IOS_TEXT_SELECTOR = '*//XCUIElementTypeStaticText';
