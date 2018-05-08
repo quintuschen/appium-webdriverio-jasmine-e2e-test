c020784e69a5e56f74d9fad156f2dbebc4ecf2ec
Quintus Chen <quintus.chen@vizexplorer.com>
2018-05-08 16:58:55 +1200

    Swipe WIP

--- support/utils.js
+++ support/utils.js
@@ -1,8 +1,29 @@
 
 export function swipe(from, to){
-    
+    device.touchPerform([
+        {
+            action: 'press',
+            options: from,
+        },
+        {
+            action: 'moveTo',
+            options: to,
+        },
+        {
+            action: 'release'
+        },
+    ]);
+    device.pause(1000);
 }
 
-export function swipeOnElement(element, direction, distance) {
+export function swipeOnElement(element, direction, percentage) {
+    let elementDimension = element.getElementSize();
+    let elementPosition = element.getLocation();
+}
 
+function getDeviceScreenCoordinates(screenSize, coordinates) {
+    return {
+        x: Math.round(screenSize.width * (coordinates.x / 100)),
+        y: Math.round(screenSize.height * (coordinates.y / 100)),
+    }
 }
